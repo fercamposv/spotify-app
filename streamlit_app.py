d1250a4bd535cb9ab710eb7021e2eb4c95042954
fercamposv <152913641+fercamposv@users.noreply.github.com>
2023-12-05 14:07:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import datetime
 import requests
-from sklearn.preprocessing import MinMaxScaler
 from PIL import Image
 
 st.set_page_config(layout="wide")
