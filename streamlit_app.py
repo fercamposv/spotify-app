8701f59e4bea8052f9464aa1ea207b633a8fda3f
fercamposv <152913641+fercamposv@users.noreply.github.com>
2023-12-05 16:31:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
-import matplotlib.pyplot as plt
 import datetime
 import geopandas as gpd
 import folium
